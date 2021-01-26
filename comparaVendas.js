68052cce626e076b27323d8f383621533226cc80
Gilberto Cunha <gilbertocunhajr@gmail.com>
2021-01-26 19:53:41 -0300

    Testes Geek Hunter - alteração arquivo Vendas - mudança de variáveis

--- comparaVendas.js
+++ comparaVendas.js
@@ -1,14 +1,14 @@
 
-let VendasDemais = [30, 12, 30, 18, 12]
+let vendasOutros = [30, 12, 30, 18, 12]
 let vendasCarlos = [20, 10, 35]
 
-ProcessaResultado(VendasDemais,vendasCarlos)
+ProcessaResultado(vendasOutros,vendasCarlos)
 
-function ProcessaResultado(resultadoVendedores,resultadoCarlos) {
+function ProcessaResultado(resultadoOutros,resultadoCarlos) {
 
-    const result = [...new Set(resultadoVendedores.sort(function(a, b){return b-a}))];
+    const result = [...new Set(resultadoOutros.sort(function(a, b){return b-a}))];
 
-    let posicaoCarlos=1
+    let posicaoCarlos = 1
 
     for (i=0;i<resultadoCarlos.length;i++) {
         for (j=0;j<result.length;j++){
